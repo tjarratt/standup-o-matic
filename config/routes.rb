790397eda0d42a953a4ee3735ffbafceab4f0a06
Tim Jarratt <tjarratt@gmail.com>
2021-08-30 00:44:34 -0400

    Cleanup routes comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails
   .application
   .routes
   .draw do
     resources :backmakers
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end

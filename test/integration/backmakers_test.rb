20dae234c4c196ed1850a52706d15cff4982ab1d
Tim Jarratt <tjarratt@gmail.com>
2021-09-01 22:44:59 +0200

    Backfill integration test for empty backmakers case

---
+++ test/integration/backmakers_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class BackMakersTest < ActionDispatch::IntegrationTest
+  test 'add new backmakers to the squad' do
+    Backmaker.all.each(&:delete)
+
+    get '/backmakers'
+    assert_response :success
+    assert_select 'p', text: "There's no one here yet 😱"
+
+    post '/backmakers', params: { backmaker: { name: 'Ursula' } }
+    follow_redirect!
+    assert_response :success
+
+    assert_equal '/backmakers', path
+    assert_equal "Let's welcome Ursula to the team", flash[:notice]
+    assert_select 'ul.backmakers' do
+      assert_select 'li', text: 'Ursula', count: 1
+    end
+  end
+end

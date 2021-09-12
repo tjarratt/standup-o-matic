822a57055187f282855fead46e277e652c679e66
Tim Jarratt <tjarratt@gmail.com>
2021-09-12 22:16:49 +0200

    Only show events that have not yet occurred

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class Event < ApplicationRecord
   has_many :standups, dependent: :nullify
+
+  def self.all_for_today
+    where('date > ?', Date.yesterday)
+  end
 end
--- test/controllers/standups_controller_test.rb
+++ test/controllers/standups_controller_test.rb
@@ -2,6 +2,28 @@
 require 'test_helper'
 
 class StandupsControllerTest < ActionDispatch::IntegrationTest
+  test 'events stay on the board until after the date they occur' do
+    Timecop.travel Date.parse('January 1 2070')
+
+    Event.create(title: 'Retrospective', date: Date.parse('January 2 2070'))
+
+    get '/standups/today'
+    assert_response :success
+    assert_select 'section#events', /Retrospective/
+
+    Timecop.travel Date.parse('January 2 2070')
+
+    get '/standups/today'
+    assert_response :success
+    assert_select 'section#events', /Retrospective/
+
+    Timecop.travel Date.parse('January 3 2070')
+
+    get '/standups/today'
+    assert_response :success
+    assert_select 'section#events', text: /Retrospective/, count: 0
+  end
+
   test 'fridays benefit from a moment of zen' do
     Timecop.freeze Date.parse('Friday') do
       get '/standups/today'

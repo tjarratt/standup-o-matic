cbf4755fc895798b5cbc5a60898daad4c940d75e
Tim Jarratt <tjarratt@gmail.com>
2021-11-21 15:35:18 +0100

    Prefer Timecop.freeze to .travel

--- test/controllers/standups_controller_test.rb
+++ test/controllers/standups_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 
 class StandupsControllerTest < ActionDispatch::IntegrationTest
   test 'events stay on the board until after the date they occur' do
-    Timecop.travel Date.parse('January 1 2070')
+    Timecop.freeze Date.parse('January 1 2070')
 
     Backmaker.create(name: 'Alice')
     Event.create(title: 'Retrospective', date: Date.parse('January 2 2070'))
@@ -12,13 +12,13 @@ class StandupsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'section#events', /Retrospective/
 
-    Timecop.travel Date.parse('January 2 2070')
+    Timecop.freeze Date.parse('January 2 2070')
 
     get '/standups/today'
     assert_response :success
     assert_select 'section#events', /Retrospective/
 
-    Timecop.travel Date.parse('January 3 2070')
+    Timecop.freeze Date.parse('January 3 2070')
 
     get '/standups/today'
     assert_response :success
